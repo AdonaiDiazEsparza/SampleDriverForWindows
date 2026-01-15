1d6d7b3ef7a342cf37d4f050d6598d74c1784e04
Donuts Diaz <159488665+AdonaiDiazEsparza@users.noreply.github.com>
2026-01-15 10:27:58 -0600

    feat: add a sample

---
+++ SimpleSample/Source.cpp
@@ -0,0 +1,25 @@
+
+// this is the import library by standard to connect with the kernel
+#include "ntddk.h"
+
+// Sample load for the driver
+void SampleUnload(PDRIVER_OBJECT DriverObject) {
+	UNREFERENCED_PARAMETER(DriverObject);
+	DbgPrint("Unload the driver");
+}
+
+// Our entry Point is the following 
+// like the main call for as entry point for the program
+// IMPORTANT: add a extern C to solve the name entry
+extern "C" NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = SampleUnload;
+
+    // Simple print Sample Driver
+	DbgPrint("Sample Driver");
+
+	return STATUS_SUCCESS;
+}
