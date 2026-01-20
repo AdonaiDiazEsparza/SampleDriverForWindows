73b7def4016dc07a7e276cfe044f95d5f0ffdb99
Donuts Diaz <159488665+AdonaiDiazEsparza@users.noreply.github.com>
2026-01-20 00:29:03 +0000

    feat: add code to detect DLL

--- CatchEventsExample/Source.cpp
+++ CatchEventsExample/Source.cpp
@@ -1,105 +1,75 @@
+#include <ntddk.h>
 
 /*
-    My notes (Ignore them):
-    These will be a kick of balls cause I dont really understand all the functions, I have one week learning how the drivers work
-    Although I need to start creating the code, so here we go and we will start doing this project.
-
-    well, on the example driver that Im basing to program this simple driver could find it on the following link: https://github.com/microsoft/Windows-driver-samples/tree/main/general/event
-    Trying to catch events from the kernel. I'll try to get events from process.
+    Code to detect when a dll is load it
 */
 
-#include <Ntifs.h>
-#include <ntddk.h>
-#include <wdm.h>
+#define DRIVER_PREFIX "==========> DRIVER_TEST: " // Prefix for the logs
 
-/*  DEFINES     */
+#define PRINT(fmt, ...) \
+    DbgPrint(DRIVER_PREFIX fmt, ##__VA_ARGS__)
 
-#define DRIVER_PREFIX "EVENT_CATCHER: "
+/* Driver Unlaod routine */
+void UnloadDriver(PDRIVER_OBJECT  DriverObject);
 
-/* By the moment are unused*/
-#define OBJECT_NAME_STRING L"\\Device\\EVENT_CATCHER"
-#define SYMBOLIC_NAME_STRING L"\\?\\EVENT_CATCHER"
+/* Routine to detect when a DLL is loaded*/
+void LoadDLLNotify(PUNICODE_STRING imageName,	HANDLE pid, PIMAGE_INFO imageInfo);
 
-/*  Routines    */
-void DriverUnloadRoutine(PDRIVER_OBJECT DriverObject); // Unload routine
-void NotifyForAProcessCreation(HANDLE ppid, HANDLE pid, BOOLEAN create); // Routine to notify when a process starts/finished.
+/* Routine to detect when a process is created/deleted */
+void NotifyForAProcessCreation(HANDLE ppid, HANDLE pid, BOOLEAN create);
 
-// Entry Point for the Driver
+/* Entry Point */
 extern "C"
 NTSTATUS DriverEntry(PDRIVER_OBJECT  DriverObject, PUNICODE_STRING RegistryPath)
 {
+    NTSTATUS status = STATUS_SUCCESS;
+
     UNREFERENCED_PARAMETER(RegistryPath);
     UNREFERENCED_PARAMETER(DriverObject);
 
-    //PDEVICE_OBJECT      deviceObject; // Pointer to Object
-    //UNICODE_STRING      ntDeviceName = RTL_CONSTANT_STRING(OBJECT_NAME_STRING); // Name for the Object 
-    //UNICODE_STRING      symbolicLinkName = RTL_CONSTANT_STRING(SYMBOLIC_NAME_STRING); // The link name for the object 'deviceObject'
-    NTSTATUS            status; // The status return 
+    PRINT("Load Driver\n");
 
-    DbgPrint(DRIVER_PREFIX "======> DRIVER ENTRY POINT\n");
+    /*status = PsSetCreateProcessNotifyRoutine(NotifyForAProcessCreation, FALSE);
 
-    /* Create object to use it */
-    //status = IoCreateDevice(
-    //    DriverObject,               // DriverObject
-    //    0,   // DeviceExtensionSize 
-    //    &ntDeviceName,              // DeviceName
-    //    FILE_DEVICE_UNKNOWN,        // DeviceType
-    //    0,// FILE_DEVICE_SECURE_OPEN,    // DeviceCharacteristics
-    //    FALSE,                      // Not Exclusive
-    //    &deviceObject               // DeviceObject
-    //);
-
-    // Check any error
-    /*if (!NT_SUCCESS(status)) {
-        KdPrint((DRIVER_PREFIX " Error Creating object: 0x%x\n", status));
+    if (!NT_SUCCESS(status))
+    {
+        PRINT("ERROR CREATING ROUTINE TO DETECT THE PROCESS (0x%X)\n", status);
         return status;
     }*/
 
-    /* Set Entry Points */
-    DriverObject->DriverUnload = DriverUnloadRoutine;
+    status = PsSetLoadImageNotifyRoutine(LoadDLLNotify);
 
-    /* Create the link name for the Device Object in the Driver*/
-    /*status = IoCreateSymbolicLink(&symbolicLinkName, &ntDeviceName);
-
-    if (!NT_SUCCESS(status)) {
-        IoDeleteDevice(deviceObject);
-        KdPrint((DRIVER_PREFIX "IoCreateSymbolicLink returned 0x%x\n", status));
-        return(status);
-    }*/
-
-    // Set the status to obtain all process
-    status = PsSetCreateProcessNotifyRoutine(NotifyForAProcessCreation, FALSE);
-
-    if (!NT_SUCCESS(status)) {
-        KdPrint((DRIVER_PREFIX "From Notify assignation routine returned 0x%x\n", status));
+    if (!NT_SUCCESS(status))
+    {
+        PRINT("ERROR CREATING ROUTINE TO DETECT THE DLL (0x%X)\n", status);
+        return status;
     }
 
-    //status = PsSetLoadImageNotifyRoutine();
+    PRINT("Driver Unloaded\n");
 
     return status;
 }
 
-// The routine for the driver unload by the moment
-void DriverUnloadRoutine(PDRIVER_OBJECT DriverObject)
+// Routine for Unload the driver
+void UnloadDriver(PDRIVER_OBJECT  DriverObject)
 {
-    DbgPrint(DRIVER_PREFIX "====>Unloading runtime driver... ");
     UNREFERENCED_PARAMETER(DriverObject);
+    PsRemoveLoadImageNotifyRoutine(LoadDLLNotify);
+    // PsSetCreateProcessNotifyRoutine(NotifyForAProcessCreation, TRUE);
 
-    /* Set the names and objects */
-    //PDEVICE_OBJECT devObj = DriverObject->DeviceObject;
-    //UNICODE_STRING symbolicLinkName;
+    PRINT("Unload Driver\n");
+}
 
-    // Remove routine to detect process
-    PsSetCreateProcessNotifyRoutine(NotifyForAProcessCreation, TRUE);
+// Routine to detect when a DLL is loaded
+void LoadDLLNotify(PUNICODE_STRING imageName,	HANDLE pid, PIMAGE_INFO imageInfo)
+{
+	UNREFERENCED_PARAMETER(imageInfo);
+	PEPROCESS process = NULL;
+	PUNICODE_STRING processName = NULL;
+	PsLookupProcessByProcessId(pid, &process);
+	SeLocateProcessImageName(process, &processName);
 
-    // Delete symbolic link name for the driver object
-    //RtlInitUnicodeString(&symbolicLinkName, SYMBOLIC_NAME_STRING);
-    //IoDeleteSymbolicLink(&symbolicLinkName);
-
-    // Delete at the last the device Object assigned  in the driver object
-    //IoDeleteDevice(devObj);
-
-    DbgPrint(DRIVER_PREFIX "Driver Unloaded... \n");
+	PRINT("%wZ (%d) loaded %wZ\n", processName, pid, imageName);
 }
 
 // Routine to display all process created
@@ -109,7 +79,7 @@ void NotifyForAProcessCreation(HANDLE ppid, HANDLE pid, BOOLEAN create)
     if (create)
     {
 
-        DbgPrint(DRIVER_PREFIX "PROCESO CREADO: \n");
+        PRINT(DRIVER_PREFIX "PROCESO CREADO: \n");
 
         PEPROCESS process = NULL; // Variable to storage the process 
         PUNICODE_STRING parentProcessName = NULL, processName = NULL; // parentProcess and process name
@@ -125,23 +95,11 @@ void NotifyForAProcessCreation(HANDLE ppid, HANDLE pid, BOOLEAN create)
         SeLocateProcessImageName(process, &processName);
 
         // print the process
-        DbgPrint(DRIVER_PREFIX "%d %wZ\n\t\t%d %wZ\n", ppid, parentProcessName, pid, processName);
+        PRINT(DRIVER_PREFIX "%d %wZ\n\t\t%d %wZ\n", ppid, parentProcessName, pid, processName);
     }
     else
     {
-        DbgPrint(DRIVER_PREFIX "ELIMINACION DE PROCESO: \n");
-        DbgPrint(DRIVER_PREFIX "Procesexos %d lost child %d\n", ppid, pid);
+        PRINT(DRIVER_PREFIX "ELIMINACION DE PROCESO: \n");
+        PRINT(DRIVER_PREFIX "Procesexos %d lost child %d\n", ppid, pid);
     }
 }
-
-// Routine to detect DLL
-//void NotifyDLLLoaded(PUNICODE_STRING imageName, HANDLE pid, PIMAGE_INFO imageInfo)
-//{
-//    UNREFERENCED_PARAMETER(imageInfo);
-//    PEPROCESS process = NULL;
-//    PUNICODE_STRING processName = NULL;
-//    PsLookupProcessByProcessId(pid, &process);
-//    SeLocateProcessImageName(process, &processName);
-//
-//    DbgPrint("%wZ (%d) loaded %wZ", processName, pid, imageName);
-//}
