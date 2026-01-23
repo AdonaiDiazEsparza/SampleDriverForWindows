6a084c0e2827157df63d3970c99ee087b02786b7
Donuts Diaz <159488665+AdonaiDiazEsparza@users.noreply.github.com>
2026-01-23 12:36:51 -0600

    feat: add code for a Simple DLL

---
+++ CatchDllLoads/DllExample/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* Header to get the functions */
+
+#ifdef HELLO_EXPORTS
+#define HELLO_API __declspec(dllexport)
+#else
+#define HELLO_API __declspec(dllimport)
+#endif
+
+extern "C" HELLO_API int Add(int a, int b);
+
---
+++ CatchDllLoads/DllExample/hola.cpp
@@ -0,0 +1,6 @@
+#include "pch.h" 
+#include "Header.h"
+
+extern "C" int Add(int a, int b) {
+	return a + b;
+}
